980bd691038b2d815d8cc3e6a8bc45d0df04fe5f
Matteo <105592696+Howard3798@users.noreply.github.com>
2023-05-16 09:38:59 +0200

    Aggiungere i file di progetto.

---
+++ BackendDelivery/FoodProvider.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Concurrent;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BackendDelivery
+{
+    class FoodProvider
+    {
+        Product _product;
+        string _name;
+        double _distance;
+        ConcurrentQueue<int> cooker;
+        string _openingTime;
+        string _closingTime;
+        List<Product> listProducts;
+        List<Product> listCookProducts;
+
+        internal FoodProvider(string Name, double Distance, string OpeningTime, string ClosingTime)
+        {
+            listProducts = new List<Product>();
+            listCookProducts = new List<Product>();
+            cooker = new ConcurrentQueue<int>();
+
+            _name = Name;
+            _distance = Distance;
+            _openingTime = OpeningTime;
+            _closingTime = ClosingTime;
+        }
+
+        internal void AddProduct(int Id, string Name, double PreparationTime, decimal Price)
+        {
+            _product = new Product(Id, Name, PreparationTime, Price);
+            listProducts.Add(_product);
+        }
+
+        internal void Order(string productName)
+        {
+            var result = searchProduct(productName);
+
+        }
+        private Product searchProduct(string product)
+        {
+            var result = listProducts.Find(item => item.Name.Equals(product));
+            return result;
+        }
+
+        internal void CookProduct(Product product)
+        {
+        }
+
+
+        #region classeProdotto
+        class Product
+        {
+            int _id;
+            string _name;
+            double _preparationTime;
+            decimal _price;
+
+            public string Name { get => _name; set => _name = value; }
+
+            internal Product(int Id, string Name, double PreparationTime, decimal Price)
+            {
+                _id = Id;
+                this.Name = Name;
+                _preparationTime = PreparationTime;
+                _price = Price;
+            }
+        }
+        #endregion
+    }
+   
+}
---
+++ ConversationJudgeOfficeManager/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Threading;
+
+namespace ConversationJudgeOfficeManager
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            char input;
+            do
+            { 
+                Console.WriteLine("Scegli la tua factory:");
+                Console.WriteLine("T - Traduzione");
+                Console.WriteLine("C - Cibo");
+                input = char.ToUpper(Console.ReadKey().KeyChar);
+                Console.WriteLine("\n");
+                switch (input)
+                {
+                    case 'T': Console.WriteLine("Hai scelto traduzione");
+                        break;
+                    case 'C':
+                        Console.WriteLine("C - Colazione");
+                        Console.WriteLine("S - Snack");
+                        Console.WriteLine("P - Pranzo");
+                        input = char.ToUpper(Console.ReadKey().KeyChar);                        
+                        switch (input)
+                        {
+                            case 'C': Console.WriteLine(" Hai scelto colazione e questo è il menu");
+                                break;
+                            case 'S':
+                                Console.WriteLine(" Hai scelto Snack e questo è il menu");
+                                break;
+                            case 'P':
+                                Console.WriteLine(" Hai scelto Pranzo e questo è il menu");
+                                
+                                break;
+                            case 'B':
+                                break;
+                        }
+                        break;                      
+                }
+            }
+            while (input != 'E');
+        }
+    }
+}
